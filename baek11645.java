ec1d4bf66c2ea267ea0f78df0911b1ae48552849
poonghyeok <vpd3302@naver.com>
2022-03-11 14:20:41 +0900

    baek11645

---
+++ baek11645.java
@@ -0,0 +1,17 @@
+package baekjoon_part;
+
+import java.util.Scanner;
+
+public class baek11645 {
+	public void mainProcess() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)(sc.next().charAt(0)));
+		sc.close();
+	}
+	
+	public static void main(String[] args) {
+		baek11645 mf = new baek11645();
+		mf.mainProcess();
+	}
+
+}
